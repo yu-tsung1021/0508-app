202ffe58cce1eebbd5ccccc44d70997941355a48
tsung <qwerty0906205016@gmail.com>
2025-05-20 22:15:25 +0800

    feat: add gh-pages for deployment and update App component
    - Added "deploy" script to package.json for deploying to GitHub Pages.
    - Updated package-lock.json to include gh-pages dependency.
    - Modified App.css to change color of .read-the-docs class.
    - Updated App.jsx to import MySlider without file extension.
    - Created MySlider.css for styling MySlider component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MySlider from './component/MySlider.jsx'
+import MySlider from './component/MySlider'
+
 function App() {
   const [count, setCount] = useState(0)
 
--- src/component/MySlider.jsx
+++ src/component/MySlider.jsx
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import './MySlider.css';
 
 function MySlider() {
     const [value, setValue] = useState(128);
