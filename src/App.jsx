35b86b4a996c93d322042a96c0a04295958cdcc5
tsung <qwerty0906205016@gmail.com>
2025-05-28 13:39:27 +0800

    feat: 重構 MyPanel 和 MySlider 元件，新增 RGB 與 HSL 轉換功能，並整合樣式

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MySlider from './component/MySlider'
-import MyPanel from './component/MyPanel'
+import MySlider from './rgb/MySlider'
+import MyPanel from './rgb/MyPanel'
 import Calculator from './component/Calculator';
 function App() {
   const [count, setCount] = useState(0);
